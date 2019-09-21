9605f3615af11d9b4b028b8989d8148468867204
alycia hall <amhall25@gmail.com>
2019-09-21 17:41:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,19 @@
 def my_collect(array)
   i=0
+<<<<<<< HEAD
   collection=[]
   while i<array.length
   collection << (yield array[i])
   i+=1
   end
   collection
+=======
+  
+  while i<array.length
+  yield array
+  i+=1
+  end
+  
+>>>>>>> f8cecb8ba7c4f80f24d9bd9c7022282b53a57ef6
 end
 
